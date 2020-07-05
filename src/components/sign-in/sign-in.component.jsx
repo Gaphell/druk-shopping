f44551883d749fde8b253a74d22733eaa1f030b3
Tenzin Gaphel <tenzingaphel@selise.ch>
2020-07-05 20:20:34 +0600

    add sing in page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../../components/form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    login = event => {
+        event.preventDefault();
+        this.setState({ email: '', password: '' })
+    }
+
+    handleChange = event => {
+        const { name, value } = event;
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>Logged in</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.login}>
+                    <FormInput
+                        name='email'
+                        type='email'
+                        value={this.state.email}
+                        handleChange={this.handleChange}
+                        label='email'
+                        required />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='password'
+                        required />
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
