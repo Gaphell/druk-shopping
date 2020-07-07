89813cae4dc729302b99cda30e8e7f7fed0cc3cf
Tenzin Gaphel <tenzingaphel@selise.ch>
2020-07-08 00:16:47 +0600

    sign up with email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     }
 
     handleChange = event => {
-        const { name, value } = event;
+        const { name, value } = event.target;
         this.setState({ [name]: value });
     }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,30 @@ const config = {
     appId: "1:559052076199:web:1b2514822a9289f1365782",
     measurementId: "G-1DECZ0FN9R"
 };
-  
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set(
+                {
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                })
+        } catch (error) {
+            console.log('error', error.message);
+        }
+    }
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
