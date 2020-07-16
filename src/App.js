a45a83e6a6db56fcd42240779ce7fa943251d85e
Tenzin Gaphel <tenzingaphel@selise.ch>
2020-07-16 16:48:58 +0600

    redirect if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShowPage from './pages/shop/shop.component';
@@ -13,7 +13,7 @@ class App extends React.Component {
     unsubscribeFromAuth = null
 
     componentDidMount() {
-      const {setCurrentUser} = this.props;
+        const {setCurrentUser} = this.props;
 
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
@@ -26,7 +26,7 @@ class App extends React.Component {
                     });
                 });
             } else {
-              setCurrentUser(userAuth);
+                setCurrentUser(userAuth);
             }
         });
     }
@@ -42,15 +42,23 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route exact path='/shop' component={ShowPage}/>
-                    <Route exact path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact path='/signin' render={() =>
+                        this.props.currentUser
+                            ? (<Redirect to={'/'}/>)
+                            : (<SignInAndSignUpPage/>)
+                    }/>
                 </Switch>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
