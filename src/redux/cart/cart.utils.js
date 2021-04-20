d6ce583eb705d637b1193e40f295f595d0636d1d
Tenzin Gaphel <tenzin606gaphel@gmail.com>
2021-04-20 13:29:31 +0600

    remove items from cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,3 +8,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     }
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 }
+
+export const clearItemFromCart = (cartItems, cartItemToRemove) => {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);  
+}
+
