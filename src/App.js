2c4be288a2041d3fae31ded2bbe2c09999ae04df
Tenzin Gaphel <tenzin606gaphel@gmail.com>
2021-04-29 21:43:28 +0600

    code refactor

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ class App extends React.Component {
                 });
             }
             setCurrentUser(userAuth);
-            addCollectionAndDocuments('collections', collectionsArray);
         });
+        addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
     }
 
     componentWillUnmount() {
